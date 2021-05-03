1e98adb33c04f51040c19fdcd0287e78ee10b0b7
son.nguyen2 <son.nguyen2@drjoy.vn>
2021-05-03 23:03:40 +0700

    4_

--- my-secrets.js
+++ my-secrets.js
@@ -2,4 +2,7 @@ const SECRET = {
   CLIENT_ID: '92380159132-vue4qiffkkoc870pcl1if3jjdt9cj945.apps.googleusercontent.com',
   CLIENT_SECRET: 'mtFRAYiO1KLdTffg9OHlyUKf',
   API_KEY: 'AIzaSyDEdP2hOha1onpxzq7eH8nWFapl9Sk_Xjk',
+  PROJECT_ID: 'main-project-308016',
+  PROJECT_NAME: 'main-project',
+  PROJECT_NUMBER: '92380159132',
 };
